8ffcab78c7b6cec9ca05f6dd66f3e8320255e50e
Onildo Aguiar <onildo.aguiar@gmail.com>
2018-04-07 23:57:10 -0300

    Initial commit

---
+++ src/main/java/com/onildoaguiar/userapi/user/UserController.java
@@ -0,0 +1,15 @@
+package com.onildoaguiar.userapi.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping("/user/signIn")
+    @ResponseBody
+    public String signIn(){
+        return "Hello World!";
+    }
+}
